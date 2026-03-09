29093a69721f180bdb349cc85f53fa6c752f3c90
kaua <kaua.jorge@docente.senai.br>
2026-03-09 12:34:10 -0300

    static ok

--- associado/views.py
+++ associado/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('<h1>SindApp</h1><p>Bem-vindo ao portal do associado!</p>')
+    # O Django buscará automaticamente dentro da pasta templates/
+    return render(request,'associado/index.html')
+
--- setup/urls.py
+++ setup/urls.py
@@ -1,9 +1,8 @@
 
 from django.contrib import admin
-from django.urls import path
-from associado.views import index
+from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',index)
+    path('',include('associado.urls'))
 ]
