aa51a878ecf4b2fa7d2941608ae7be4d83fb2d3c
romulodf.cesar <romulodf.cesar@gmail.com>
2026-03-10 18:16:53 -0300

    finalizei front-end

--- associado/views.py
+++ associado/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import render
 def index(request):
     # O Django buscará automaticamente dentro da pasta templates/
     return render(request,'associado/index.html')
-
 # Função para carregar o perfil do associado
 def perfil(request):
     return render(request, 'associado/perfil.html')
