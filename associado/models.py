eff68250411aeab773713e73cb5050037931f05c
romulodf-cesar <romulodf.cesar@gmail.com>
2026-03-19 22:06:21 -0300

    ok

--- associado/models.py
+++ associado/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 
 # Create your models here.
-class Associado(models.Model):     
-    nome = models.CharField(max_length=100,null=False,blank=False)
-    ativo = models.BooleanField(default=True,null=False,blank=False)
+
+
+class Associado(models.Model):
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    ativo = models.BooleanField(default=True, null=False, blank=False)
+
     def __str__(self):
         return self.nome+" - Ativo: " + str(self.ativo)
- 
+    
+class Empresa(models.Model):
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    cnpj = models.CharField(max_length=14, null=False, blank=False)
+    
+    def __str__(self):
+        return self.nome+" - CNPJ: " + self.cnpj
